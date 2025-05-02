8ce8c272a835cbbbbd3f672c436eb981a525eae4
debojeetbose69 <debojeetbose69@gmail.com>
2025-05-02 19:03:21 +0000

    CORS issues

--- ems/src/main/java/com/example/ems/config/SecurityConfig.java
+++ ems/src/main/java/com/example/ems/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.example.ems.config;
 
 import com.example.ems.service.UserService;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
 import org.springframework.web.cors.CorsConfiguration;
 import java.util.Arrays;
 
@@ -20,13 +23,19 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .cors(cors -> cors.configurationSource(request -> {
-                CorsConfiguration config = new CorsConfiguration();
-                config.setAllowedOrigins(Arrays.asList("http://localhost:3000","https://full-stack-10oihg7sh-debojeet-bose-s-projects.vercel.app"));
-                config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
-                config.setAllowCredentials(true);
-                return config;
-            }))
+            .cors(cors -> cors.configurationSource(request -> { CorsConfiguration config = new CorsConfiguration();
+                         List<String> allowedOrigins = Arrays.asList("http://localhost:3000", "https://full-stack-10oihg7sh-debojeet-bose-s-projects.vercel.app");
+                         config.setAllowedOrigins(allowedOrigins);
+                         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+                         config.setAllowedHeaders(Arrays.asList("*"));
+                         config.setExposedHeaders(Arrays.asList("*"));
+                         String origin = request.getHeader("Origin");
+                         if(origin != null && allowedOrigins.contains(origin)) {
+                             config.setAllowCredentials(true);
+                         }
+
+                         return config;
+                     }))
             .oauth2Login(oauth2 -> oauth2
                 .userInfoEndpoint(userInfo -> userInfo.userService(oauth2UserService))
                 .loginPage("/login")
